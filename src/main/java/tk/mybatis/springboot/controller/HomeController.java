41720295382d3bcf4f8813e3a75e3e60a591cf2a
chen_anguo <cag2050@163.com>
2019-08-16 16:21:59 +0800

    add: swagger2

--- src/main/java/tk/mybatis/springboot/controller/HomeController.java
+++ src/main/java/tk/mybatis/springboot/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
         return "首页";
     }
--- src/main/java/tk/mybatis/springboot/controller/UserController.java
+++ src/main/java/tk/mybatis/springboot/controller/UserController.java
@@ -1,14 +1,11 @@
 package tk.mybatis.springboot.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import tk.mybatis.springboot.model.User;
+import org.springframework.web.bind.annotation.*;
 import tk.mybatis.springboot.mapper.UserMapper;
+import tk.mybatis.springboot.model.User;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -16,29 +13,29 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
-    @RequestMapping("/getUsers")
+    @RequestMapping(value = "/getUsers", method = RequestMethod.POST)
     public List<User> getUsers() {
         List<User> users = userMapper.getAll();
         return users;
     }
 
-    @RequestMapping("/getUser")
-    public User getUser(Long id) {
+    @RequestMapping(value = "/getUser", method = RequestMethod.POST)
+    public User getUser(@RequestParam Long id) {
         User user = userMapper.getOne(id);
         return user;
     }
 
-    @RequestMapping("/add")
-    public void save(User user) {
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public void save(@RequestBody User user) {
         userMapper.insert(user);
     }
 
-    @RequestMapping(value = "update")
-    public void update(User user) {
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public void update(@RequestBody User user) {
         userMapper.update(user);
     }
 
-    @RequestMapping(value = "/delete/{id}")
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
     public void delete(@PathVariable("id") Long id) {
         userMapper.delete(id);
     }
